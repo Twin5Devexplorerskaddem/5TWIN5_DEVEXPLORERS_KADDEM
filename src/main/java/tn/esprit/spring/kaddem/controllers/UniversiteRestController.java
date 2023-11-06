6f6b5c38350f572deb220c6d2ced5f4ba21910bb
Zeineb Hamdi <61847277+zeineeb@users.noreply.github.com>
2023-11-06 22:31:17 +0100

    AcrossOrigin

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -12,23 +12,27 @@ import java.util.Set;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/universite")
 public class UniversiteRestController {
 	@Autowired
 	IUniversiteService universiteService;
 	// http://localhost:8089/Kaddem/universite/retrieve-all-universites
+	@CrossOrigin(origins = "*")
 	@GetMapping("/retrieve-all-universites")
 	public List<Universite> getUniversites() {
 		List<Universite> listUniversites = universiteService.retrieveAllUniversites();
 		return listUniversites;
 	}
 	// http://localhost:8089/Kaddem/universite/retrieve-universite/8
+	@CrossOrigin(origins = "*")
 	@GetMapping("/retrieve-universite/{universite-id}")
 	public Universite retrieveUniversite(@PathVariable("universite-id") Integer universiteId) {
 		return universiteService.retrieveUniversite(universiteId);
 	}
 
 	// http://localhost:8089/Kaddem/universite/add-universite
+	@CrossOrigin(origins = "*")
 	@PostMapping("/add-universite")
 	public Universite addUniversite(@RequestBody Universite u) {
 		Universite universite = universiteService.addUniversite(u);
@@ -36,12 +40,14 @@ public class UniversiteRestController {
 	}
 
 	// http://localhost:8089/Kaddem/universite/remove-universite/1
+	@CrossOrigin(origins = "*")
 	@DeleteMapping("/remove-universite/{universite-id}")
 	public void removeUniversite(@PathVariable("universite-id") Integer universiteId) {
 		universiteService.deleteUniversite(universiteId);
 	}
 
 	// http://localhost:8089/Kaddem/universite/update-universite
+	@CrossOrigin(origins = "*")
 	@PutMapping("/update-universite")
 	public Universite updateUniversite(@RequestBody Universite u) {
 		Universite u1= universiteService.updateUniversite(u);
