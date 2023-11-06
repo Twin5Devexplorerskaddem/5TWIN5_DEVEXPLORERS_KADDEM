ecddd245fe1dfcb31a83add1173f6dd176f4995d
amine7-rgb <mohamedamine.eloudi@esprit.tn>
2023-11-06 19:23:40 +0100

    Test1

--- src/test/java/tn/esprit/spring/kaddem/SansMockTest.java
+++ src/test/java/tn/esprit/spring/kaddem/SansMockTest.java
@@ -1,125 +1,150 @@
 package tn.esprit.spring.kaddem;
 
 
+import com.mysql.cj.xdevapi.Client;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import tn.esprit.spring.kaddem.entities.DetailEquipe;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import tn.esprit.spring.kaddem.services.DetailEquipeServiceImpl;
+import tn.esprit.spring.kaddem.services.IDetailEquipe;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import java.util.List;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class SansMockTest {
-    @Autowired
-    private WebApplicationContext context;
 
-    private MockMvc mockMvc;
+    @Autowired
+    private IDetailEquipe detailEquipeService;
 
     @Before
     public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
-
-    @Test
-    public void testGetEquipes() throws Exception {
-        // Effectuez une requête GET sur votre endpoint
-        mockMvc.perform(get("/Detailequipe/retrieve-all-detailequipes")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(5))) // Vérifiez que vous avez 2 éléments dans la liste (ajustez le cas échéant)
-                .andExpect(jsonPath("[0].idDetailEquipe", is(6))) // Vérifiez les détails des éléments
-                .andExpect(jsonPath("[0].salle", is(258)))
-                .andExpect(jsonPath("[0].thematique", is("Sample Thematique")))
-                .andExpect(jsonPath("[1].idDetailEquipe", is(5)))
-                .andExpect(jsonPath("[1].salle", is(123)));
-    }
-
-    @Test
-    public void testAddEquipe() throws Exception {
-        DetailEquipe equipeToAdd = new DetailEquipe();
-        equipeToAdd.setSalle(258);
-        equipeToAdd.setThematique("Sample Thematique");
-
-        // Convertir l'objet en JSON
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(equipeToAdd);
-
-        mockMvc.perform(post("/Detailequipe/add-detailequipe")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.salle").value(258))
-                .andExpect(jsonPath("$.thematique").value("Sample Thematique"));
-    }
-
-    @Test
-    public void testRemoveEquipe() throws Exception {
-        Integer equipeId = 12; // Remplacez par l'ID de l'équipe que vous souhaitez supprimer
-
-        mockMvc.perform(delete("/Detailequipe/remove-detailequipe/{idDetailEquipe}", equipeId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        // Vous pouvez ajouter des assertions supplémentaires ici pour vérifier que l'équipe a été supprimée avec succès, par exemple, en essayant de la récupérer et en vous assurant qu'elle n'existe pas.
-
-        // Par exemple, vous pouvez ajouter un test pour vérifier que l'équipe a été supprimée en essayant de la récupérer et en vous assurant qu'elle n'existe pas.
-//        mockMvc.perform(get("/Detailequipe/retrieve-detailequipe/{idDetailEquipe}", equipeId)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isNotFound()); // Vérifiez que l'équipe n'existe pas.
-
-        // Assurez-vous que l'équipe a été supprimée avec succès en vérifiant son absence dans la base de données ou en effectuant d'autres validations appropriées.
-    }
-
-
 
 //    @Test
-//    public void testRemoveEquipe() throws Exception {
-//        Integer equipeId = 1; // Remplacez par l'ID de l'équipe que vous souhaitez supprimer
+//    public void testAddDetailEquipe()throws ParseException{
 //
-//        mockMvc.perform(delete("/Detailequipe/remove-detailequipe/{idDetailEquipe}", equipeId)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+//        Date  dat =dateFormat.parse("30/02/2000");
+//        Client c = new Client("dd","dd");
+//        Client client =clientservice.addclient(c);
+//        System.out.print("client "+client);
+//        assertNotNull(client.getIdClient());
+//        assertNotNull(client.getCategorieClient());
+//        assertNotNull(client.getNom().length()>0);
+//        clientService.deleteClient(client.getIdClient())
 //
-//        // Vérifiez ici que l'équipe a été supprimée avec succès, par exemple, en essayant de la récupérer et en vous assurant qu'elle n'existe pas.
+//
+//
+//
 //    }
 
 
     @Test
-    public void testUpdateEquipe() throws Exception {
-        // Créez une équipe avec les modifications que vous souhaitez apporter
-        DetailEquipe updatedEquipe = new DetailEquipe();
-        updatedEquipe.setIdDetailEquipe(5); // ID de l'équipe à mettre à jour
-        updatedEquipe.setSalle(14789); // Nouvelle salle
-        updatedEquipe.setThematique("difficile Thematique"); // Nouvelle thématique
+    public void testAddEquipe() {
+        // Créez un objet DetailEquipe pour le test
+        DetailEquipe equipe = new DetailEquipe();
+        equipe.setSalle(1); // Remplacez 1 par la valeur appropriée
+        equipe.setThematique("Thématique de test"); // Remplacez par la thématique appropriée
 
-        // Convertissez l'objet en JSON
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(updatedEquipe);
+        // Appelez la méthode d'ajout de l'équipe de votre service
+        DetailEquipe addedEquipe = detailEquipeService.addEquipe(equipe);
 
-        mockMvc.perform(put("/Detailequipe/update-detailequipe")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isOk());
+        // Vérifiez si l'équipe ajoutée n'est pas nulle
+        assertNotNull(addedEquipe);
 
+        // Vérifiez si l'ID de l'équipe ajoutée n'est pas nul
+        assertNotNull(addedEquipe.getIdDetailEquipe());
 
+        // Vérifiez si les propriétés de l'équipe ajoutée correspondent à celles que vous avez définies
+//        assertEquals(17, 258); // Remplacez 1 par la valeur appropriée
+//        assertEquals("Thématique de test", addedEquipe.getThematique()); // Remplacez par la thématique appropriée
+
+        // Si nécessaire, vous pouvez supprimer l'équipe ajoutée après le test
+        // detailEquipeService.deleteEquipe(addedEquipe.getIdDetailEquipe());
     }
+
+
+
+
+    @Test
+    public void testGetAllEquipes() {
+        // Appelez la méthode de l'API pour récupérer toutes les équipes
+        List<DetailEquipe> listEquipes = detailEquipeService.retrieveAllDetailEquipes();
+
+        // Vérifiez si la liste n'est pas nulle
+        assertNotNull(listEquipes);
+
+        // Vérifiez si la liste n'est pas vide
+        assertFalse(listEquipes.isEmpty());
+
+        // Vous pouvez effectuer d'autres assertions pour vérifier les détails des équipes récupérées si nécessaire
+        // assertEquals(expectedSize, listEquipes.size());
+        // assertEquals(expectedThematique, listEquipes.get(0).getThematique());
+        // ...
+
+        // Assurez-vous de gérer la pagination ou la taille attendue de la liste, selon le comportement de votre API.
+    }
+
+
+
+
+    @Test
+    public void testRemoveEquipe() {
+        // Créez un objet DetailEquipe pour le test et ajoutez-le à la base de données
+        DetailEquipe equipe = new DetailEquipe();
+        // Initialisez les propriétés de l'équipe selon vos besoins
+        equipe = detailEquipeService.addEquipe(equipe);
+
+        // Récupérez l'ID de l'équipe ajoutée
+        Integer equipeId = equipe.getIdDetailEquipe();
+
+        // Appelez la méthode de l'API pour supprimer l'équipe
+        detailEquipeService.deleteEquipe(equipeId);
+
+//        // Vérifiez si l'équipe a été correctement supprimée en essayant de la récupérer à nouveau
+//        DetailEquipe deletedEquipe = detailEquipeService.retrieveEquipe(14);
+//
+//        // Assurez-vous que l'équipe récupérée est nulle pour vérifier qu'elle a été supprimée avec succès
+//        assertNull(deletedEquipe);
+    }
+
+
+    @Test
+    public void testUpdateEquipe() {
+        // Créez un objet DetailEquipe pour le test et ajoutez-le à la base de données
+        DetailEquipe equipe = new DetailEquipe();
+        // Initialisez les propriétés de l'équipe selon vos besoins
+        equipe = detailEquipeService.addEquipe(equipe);
+
+        // Modifiez les propriétés de l'équipe
+        equipe.setSalle(2); // Nouvelle valeur pour salle
+        equipe.setThematique("Nouvelle Thématique"); // Nouvelle valeur pour thématique
+
+        // Appelez la méthode de l'API pour mettre à jour l'équipe
+        DetailEquipe updatedEquipe = detailEquipeService.updateEquipe(equipe);
+
+        // Récupérez l'ID de l'équipe mise à jour
+        Integer equipeId = updatedEquipe.getIdDetailEquipe();
+
+        // Récupérez l'équipe à partir de la base de données pour vérifier si elle a été correctement mise à jour
+//        DetailEquipe retrie   vedEquipe = detailEquipeService.retrieveEquipe(equipeId);
+
+        // Vérifiez si l'équipe mise à jour est égale à celle récupérée de la base de données
+//        assertEquals(updatedEquipe, retrievedEquipe);
+
+        // Vérifiez si les propriétés de l'équipe mise à jour correspondent aux nouvelles valeurs
+//        assertEquals(2, updatedEquipe.getSalle());
+//        assertEquals("Nouvelle Thématique", updatedEquipe.getThematique());
+    }
+
+
 }
