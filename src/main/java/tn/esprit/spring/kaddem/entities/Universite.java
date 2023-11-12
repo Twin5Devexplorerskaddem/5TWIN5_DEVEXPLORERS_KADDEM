5c13b47cad3ef22205c8c1d8a255e6753fc2306d
Zeineb Hamdi <61847277+zeineeb@users.noreply.github.com>
2023-11-12 01:44:04 +0100

    update

--- src/main/java/tn/esprit/spring/kaddem/entities/Universite.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Universite.java
@@ -1,8 +1,6 @@
 package tn.esprit.spring.kaddem.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -11,8 +9,6 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Universite implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +18,63 @@ public class Universite implements Serializable {
     private String description;  // Add description attribute
     private String email;        // Add email attribute
 
+    public Universite() {
+    }
+
+    public Universite(String nomUniv, String localisation, String description, String email) {
+        super();
+        this.nomUniv = nomUniv;
+        this.localisation = localisation;
+        this.description = description;
+        this.email = email;
+    }
+
+    public Universite(Integer idUniv, String nomUniv, String localisation, String description, String email) {
+        super();
+        this.idUniv = idUniv;
+        this.nomUniv = nomUniv;
+        this.localisation = localisation;
+        this.description = description;
+        this.email = email;
+    }
+
+    public Integer getIdUniv() {
+        return idUniv;
+    }
+
+    public void setIdUniv(Integer idUniv) {
+        this.idUniv = idUniv;
+    }
+
+    public String getNomUniv() {
+        return nomUniv;
+    }
+
+    public void setNomUniv(String nomUniv) {
+        this.nomUniv = nomUniv;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
